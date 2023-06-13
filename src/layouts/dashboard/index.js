dfd1f0dfb781cb920a463b8a85db6b4f93f9a212
Promit <Promitbodak@gmail.com>
2023-06-14 00:13:21 +0530

    chat & conversation section is build

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -67,7 +67,7 @@ const DashboardLayout = () => {
   const { onToggleMode } = useSettings();
 
   return (
-    <>
+    <Stack direction="row">
       <Box
         p={2}
         sx={{
@@ -180,7 +180,7 @@ const DashboardLayout = () => {
         </Stack>
       </Box>
       <Outlet />
-    </>
+    </Stack>
   );
 };
 
